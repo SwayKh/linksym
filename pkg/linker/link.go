79793643fe0115f693e0901903cb4739b7507c91
SwayKh <khatrisumit712@gmail.com>
2024-09-08 18:01:39 +0530

    Move Symlinking logic to linker package. Add getting working directory logic to separate function

---
+++ pkg/linker/link.go
@@ -0,0 +1,69 @@
+package linker
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func Link(sourcePath, linkPath string) error {
+	// Get File info, to check if it exists, and if it's a directory or not
+	fileInfo, err := os.Stat(sourcePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return errors.New("File path provided doesn't exist")
+		} else {
+			return err
+		}
+	}
+
+	// If path is a directory, Rename ii
+	if fileInfo.IsDir() {
+		err = os.Rename(sourcePath, linkPath)
+		if err != nil {
+			return fmt.Errorf("Couldn't rename directory %s to %s\n %w", sourcePath, linkPath, err)
+		}
+	} else {
+		// If Path is a file, copy it to new path, and delete original
+		err = moveFile(sourcePath, linkPath)
+		if err != nil {
+			return err
+		}
+	}
+	err = os.Symlink(linkPath, sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't create symlink %s\n %w", linkPath, err)
+	}
+
+	return nil
+}
+
+func moveFile(source, destination string) error {
+	src, err := os.Open(source)
+	if err != nil {
+		return fmt.Errorf("Failed to source file: %s\n %w", source, err)
+	}
+	defer src.Close()
+
+	dst, err := os.Create(destination)
+	if err != nil {
+		return fmt.Errorf("Failed to create file %s\n %w", destination, err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return fmt.Errorf("Failed to copy file %s to %s\n %w", source, destination, err)
+	}
+
+	err = os.Remove(source)
+	if err != nil {
+		if os.IsPermission(err) {
+			return fmt.Errorf("Failed to Remove file %s\n Please run with elevated privileges\n", source)
+		} else {
+			return fmt.Errorf("Failed to Remove file %s\n %w", source, err)
+		}
+	}
+	return nil
+}
