ce24f516fb3b26ea90d2abeb57c82b2f9e9d6058
SwayKh <khatrisumit712@gmail.com>
2024-09-10 17:00:24 +0530

    Update config package, Fix error returns, decouple the Saving config and Updating records logic, Use better Variable names

--- pkg/config/io.go
+++ pkg/config/io.go
@@ -2,17 +2,62 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 func CheckFile(path string) (bool, os.FileInfo, error) {
 	fileInfo, err := os.Stat(path)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			return false, nil, errors.New("File path provided doesn't exist")
+			return false, nil, fmt.Errorf("File %s doesn't exist", path)
 		} else {
-			return false, nil, err
+			return false, nil, fmt.Errorf("Error getting file info: %w", err)
 		}
 	}
 	return true, fileInfo, nil
+}
+
+func loadConfig(configPath string) (appConfig, error) {
+	// Check if config file exists
+	if fileExists, _, err := CheckFile(configPath); err != nil {
+		return appConfig{}, fmt.Errorf("Error checking if .linksym.yaml exists: %w", err)
+	} else if !fileExists {
+		return appConfig{}, fmt.Errorf("No .linksym.yaml file found, please run linksym init: %w", err)
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return appConfig{}, fmt.Errorf("Error opening config file: %s: %w", configPath, err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return appConfig{}, fmt.Errorf("Error reading data from config file: %w", err)
+	}
+
+	config := appConfig{}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return appConfig{}, fmt.Errorf("Error loading data to appConfig{}: %w", err)
+	}
+	return config, nil
+}
+
+func writeConfig(configuration appConfig) error {
+	data, err := yaml.Marshal(&configuration)
+	if err != nil {
+		return fmt.Errorf("Error marshalling data from appConfig{}: %w", err)
+	}
+
+	err = os.WriteFile(ConfigPath, data, 0o644)
+	if err != nil {
+		return fmt.Errorf("Error writing record to config file: %w", err)
+	}
+	return nil
 }
