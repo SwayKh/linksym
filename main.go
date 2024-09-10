a2a6ecdff86dbf2fbc3be3e6f298f18646852a65
SwayKh <khatrisumit712@gmail.com>
2024-09-10 20:12:09 +0530

    Move the subcommands logic to a separate file, and move all logic of linking from main.go to Add subcommand function

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/SwayKh/linksym/cmd"
-	"github.com/SwayKh/linksym/pkg/config"
-	"github.com/SwayKh/linksym/pkg/linker"
 )
 
 func main() {
@@ -30,42 +26,11 @@ func main() {
 	case "init":
 		cmd.Init()
 	case "add":
-		cmd.Add()
+		cmd.Add(args[1:])
 	case "remove":
 		cmd.Remove()
 	default:
 		fmt.Println("Unknown subcommand")
 		cmd.Help()
-	}
-
-	if fileExists, _, err := config.CheckFile("./.linksym.yaml"); err != nil {
-		fmt.Println("Error checking if .linksym.yaml exists")
-	} else if !fileExists {
-		fmt.Println("No .linksym.yaml file found, please run linksym init")
-		os.Exit(1)
-	}
-
-	err := config.InitialiseConfig()
-	if err != nil {
-		// fmt.Errorf("Error initialising config: %s", err)
-		fmt.Println(err)
-	}
-
-	cfg, err := config.LoadConfig("./.linksym.yaml")
-	if err != nil {
-		// fmt.Errorf("Error loading config: %s", err)
-		fmt.Println(err)
-	}
-
-	sourcePath := os.Args[1]
-	filename := filepath.Base(sourcePath)
-	// destinationPath := os.Args[2]
-	destinationPath := cfg.InitDirectory + "/" + filename
-
-	fmt.Println(sourcePath, filename, destinationPath)
-
-	err = linker.Link(sourcePath, destinationPath, config.ConfigPath)
-	if err != nil {
-		fmt.Println(err)
 	}
 }
