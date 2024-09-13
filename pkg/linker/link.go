0a685a76caa27c77d51917cb5e7e113578d55e7a
SwayKh <khatrisumit712@gmail.com>
2024-09-13 21:08:43 +0530

    Create MoveAndLink function and Update errors for Linking logic and MoveFile function
    Separate the Linking logic to a Link function.
    The new MoveAndLink function handles moving the files, renaming the
    directories and then calls the Link Function
    This allows for the special case of Dest file existing and source file
    not existing to be handle by calling the Link function with reversed
    arguments.
    This Should work theoretically, Needs testing.

--- pkg/linker/link.go
+++ pkg/linker/link.go
@@ -8,23 +8,34 @@ import (
 	"github.com/SwayKh/linksym/pkg/config"
 )
 
-func Link(sourcePath string, destinationPath string, isDirectory bool, toMove bool) error {
+func MoveAndLink(sourcePath, destinationPath string, isDirectory bool) error {
 	// If path is a directory, Rename it
 	if isDirectory {
 		err := os.Rename(sourcePath, destinationPath)
 		if err != nil {
-			return fmt.Errorf("Couldn't rename directory %s to %s\n %w", sourcePath, destinationPath, err)
+			return fmt.Errorf("Couldn't rename directory %s to %s: %w", sourcePath, destinationPath, err)
 		}
-	} else if toMove {
-		// If Path is a file, copy it to new path, and delete original
+	} else {
+		// If path is a file, create a file at new location, copy it over, and
+		// delete original file. This method allows better handling when linking
+		// across file system than just renaming files
 		err := moveFile(sourcePath, destinationPath)
 		if err != nil {
 			return err
 		}
 	}
+
+	err := Link(sourcePath, destinationPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Link(sourcePath, destinationPath string) error {
 	err := os.Symlink(destinationPath, sourcePath)
 	if err != nil {
-		return fmt.Errorf("Couldn't create symlink %s\n %w", destinationPath, err)
+		return fmt.Errorf("Couldn't create symlink %s: %w", destinationPath, err)
 	}
 
 	err = config.AddRecord(sourcePath, destinationPath)
@@ -37,19 +48,19 @@ func Link(sourcePath string, destinationPath string, isDirectory bool, toMove bo
 func moveFile(source, destination string) error {
 	src, err := os.Open(source)
 	if err != nil {
-		return fmt.Errorf("Failed to source file: %s\n %w", source, err)
+		return fmt.Errorf("Failed to source file: %s: %w", source, err)
 	}
 	defer src.Close()
 
 	dst, err := os.Create(destination)
 	if err != nil {
-		return fmt.Errorf("Failed to create file %s\n %w", destination, err)
+		return fmt.Errorf("Failed to create file %s: %w", destination, err)
 	}
 	defer dst.Close()
 
 	_, err = io.Copy(dst, src)
 	if err != nil {
-		return fmt.Errorf("Failed to copy file %s to %s\n %w", source, destination, err)
+		return fmt.Errorf("Failed to copy file %s to %s: %w", source, destination, err)
 	}
 	err = dst.Sync()
 	if err != nil {
@@ -59,9 +70,9 @@ func moveFile(source, destination string) error {
 	err = os.Remove(source)
 	if err != nil {
 		if os.IsPermission(err) {
-			return fmt.Errorf("Failed to Remove file %s\n Please run with elevated privileges\n", source)
+			return fmt.Errorf("Failed to Remove file %s\n Please run with elevated privileges", source)
 		} else {
-			return fmt.Errorf("Failed to Remove file %s\n %w", source, err)
+			return fmt.Errorf("Failed to Remove file %s: %w", source, err)
 		}
 	}
 	return nil
