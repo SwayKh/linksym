14f5cfb94aee07f43eae8ff879dacfba4f3608f3
SwayKh <khatrisumit712@gmail.com>
2024-09-19 23:09:17 +0530

    Clean up main.go and move all the logic to cmd/linksym.go and have every function return the error to main

---
+++ cmd/linksym.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/SwayKh/linksym/pkg/config"
+)
+
+func Run() error {
+	var err error
+
+	CreateFlags()
+	flag.Parse()
+
+	// Since the Init Command creates the config file the LoadConfig function
+	// can't be called before handling the init subcommand.
+	if flag.Arg(0) == "init" {
+		return Init()
+	}
+
+	err = config.SetupDirectories()
+	if err != nil {
+		return err
+	}
+
+	err = config.LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	if *HelpFlag {
+		Help()
+		os.Exit(0)
+	}
+
+	switch flag.Arg(0) {
+	case "":
+		Help()
+	case "init":
+		break
+	case "add":
+		return Add(os.Args[2:])
+	case "remove":
+		return Remove(os.Args[2:])
+	case "source":
+		return Source()
+	default:
+		return fmt.Errorf("Invalid Command. Please use -h or --help flags to see available commands.")
+	}
+
+	if err := config.WriteConfig(); err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,74 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/SwayKh/linksym/cmd"
-	"github.com/SwayKh/linksym/pkg/config"
 )
 
 func main() {
-	var err error
-	cmd.CreateFlags()
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) < 1 {
-		cmd.Help()
-	}
-
-	if *cmd.HelpFlag {
-		cmd.Help()
-	}
-
-	if args[0] == "init" {
-		err = cmd.Init()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-
-	err = config.SetupDirectories()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	err = config.LoadConfig()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	subcommand := args[0]
-
-	// This mess is created since the Config needs to be loaded at startup, which
-	// check if the configuration file is present or not, which doesn't won't
-	// exist until the init function is called, so the LoadConfig function needs
-	// to be called after a "init" subcommand call
-	switch subcommand {
-	case "init":
-		break
-	case "add":
-		err = cmd.Add(args[1:])
-	case "remove":
-		err = cmd.Remove(args[1:])
-	case "source":
-		err = cmd.Source()
-	default:
-		fmt.Println("Unknown subcommand")
-		cmd.Help()
-	}
-
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	if err := config.WriteConfig(); err != nil {
+	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
