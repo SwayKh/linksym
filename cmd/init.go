28471edadc257196c7b6852f2f94fa011f940b32
SwayKh <khatrisumit712@gmail.com>
2024-09-20 23:37:20 +0530

    Update Init subcommand to require config Name as argument to pass to InitialiseConfig()

--- cmd/init.go
+++ cmd/init.go
@@ -3,8 +3,8 @@ package cmd
 import "github.com/SwayKh/linksym/pkg/config"
 
 // Initialise and empty config with cwd as init directory
-func Init() error {
-	err := config.InitialiseConfig()
+func Init(configName string) error {
+	err := config.InitialiseConfig(configName)
 	if err != nil {
 		return err
 	}
