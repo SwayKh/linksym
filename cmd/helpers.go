d32fefaf0a83f52c8dfc82ae881c8744d58e8f9c
SwayKh <khatrisumit712@gmail.com>
2024-09-20 14:08:50 +0530

    Update filePathInfo function to return a struct, instead of 5 return values. Update References in Add and Remove Commands

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -9,19 +9,35 @@ import (
 	"github.com/SwayKh/linksym/pkg/config"
 )
 
+type fileInfo struct {
+	AbsPath  string
+	Exists   bool
+	Info     os.FileInfo
+	IsDir    bool
+	HasSlash bool
+}
+
 // Handle the repeating function calls in one place
-func filePathInfo(path string) (absPath string, exists bool, info os.FileInfo, hasSlash bool, err error) {
+func filePathInfo(path string) (info fileInfo, err error) {
+	info = fileInfo{}
+
 	if strings.HasSuffix(path, string(os.PathSeparator)) {
-		hasSlash = true
-	}
-	absPath, err = filepath.Abs(path)
-	if err != nil {
-		return "", false, nil, false, fmt.Errorf("Error getting absolute path of file %s", path)
+		info.HasSlash = true
 	}
 
-	exists, info, err = config.CheckFile(absPath)
+	info.AbsPath, err = filepath.Abs(path)
 	if err != nil {
-		return "", false, nil, false, err
+		return fileInfo{}, fmt.Errorf("Error getting absolute path of file %s", path)
 	}
-	return absPath, exists, info, hasSlash, nil
+
+	info.Exists, info.Info, err = config.CheckFile(info.AbsPath)
+	if err != nil {
+		return fileInfo{}, err
+	}
+
+	if info.Info.IsDir() {
+		info.IsDir = true
+	}
+
+	return info, nil
 }
