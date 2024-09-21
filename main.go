a073274fb66a16b9f3e059c1ec51f655f15e85f7
SwayKh <khatrisumit712@gmail.com>
2024-09-21 16:17:05 +0530

    Move the Run() Logic from linksym.go to main.go again

--- main.go
+++ main.go
@@ -1,15 +1,74 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/SwayKh/linksym/cmd"
+	"github.com/SwayKh/linksym/pkg/config"
 )
 
+
 func main() {
-	if err := cmd.Run(); err != nil {
+	if err := Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+// Load config, Setup up Global variables and handle all subcommand switching
+func Run() error {
+	cmd.CreateFlags()
+	flag.Parse()
+
+	configName := ".linksym.yaml"
+
+	err := InitialiseHomePath()
+	if err != nil {
+		return err
+	}
+
+	// Since the Init Command creates the config file, the LoadConfig function
+	// can't be called before handling the init subcommand.
+	// But Init function calls aliasPath, which requires HomeDirectory variable,
+	// and hence function SetupDirectories was split up
+	if flag.Arg(0) == "init" {
+		return cmd.Init(configName)
+	}
+
+	configuration, err := config.LoadConfig(configName)
+	if err != nil {
+		return err
+	}
+	SetupDirectories(configuration, configName)
+
+	if *cmd.HelpFlag {
+		cmd.Help()
+		os.Exit(0)
+	}
+
+	switch flag.Arg(0) {
+	case "":
+		cmd.Help()
+	case "init":
+		break
+	case "add":
+		err = cmd.Add(configuration, os.Args[2:])
+	case "remove":
+		err = cmd.Remove(configuration, os.Args[2:])
+	case "source":
+		err = cmd.Source()
+	default:
+		err = fmt.Errorf("Invalid Command. Please use -h or --help flags to see available commands.")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if err := config.WriteConfig(configuration, configName); err != nil {
+		return err
+	}
+	return nil
 }
