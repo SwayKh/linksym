236b833bf5e9ca7813d29c2fd7883fba94a9847b
SwayKh <khatrisumit712@gmail.com>
2024-09-22 20:02:28 +0530

    Create a working Source Subcommand.
    Simple subcommand, Get's Paths from Configuration Records, creates
    destination directory and Run Add Subcommand under the hood without
    updating configuration Records

---
+++ cmd/source.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/SwayKh/linksym/pkg/config"
+)
+
+// Loop over the configuration []Records, for each entry get the source and
+// destination paths. Run the Link command for each entry.
+func Source(configuration *config.AppConfig) error {
+	for _, record := range configuration.Records {
+		sourcePath := record.Paths[0]
+		destinationPath := filepath.Dir(record.Paths[1])
+
+		err := os.MkdirAll(destinationPath, 0o755)
+		if err != nil {
+			return err
+		}
+
+		pathArgs := []string{sourcePath, destinationPath}
+
+		err = Add(configuration, pathArgs, false)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
