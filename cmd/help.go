90a5f5a8b322a0fc6a42d293377431b46f719055
SwayKh <khatrisumit712@gmail.com>
2024-09-22 22:28:41 +0530

    Update Help message with all subcommands and using a raw string, decreasing multiple fmt.Println() calls

--- cmd/help.go
+++ cmd/help.go
@@ -1,19 +1,29 @@
 package cmd
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func Help() {
-	fmt.Println("Usage: linksym [subcommand] [flags]")
+	usage := ` Usage: linksym [subcommand] [flags]
 
-	fmt.Println("\n Subcommands:")
-	fmt.Println("   add [Path] [(optional) Destination]:")
-	fmt.Println("     Create a symlink for given path, optionally define a destination for symlink")
-	fmt.Println("   remove [Path]")
-	fmt.Println("     Remove the symlink and move the file to the original path")
+Subcommands:
+  init [flags]
+    Initialize the linksym configuration file (.linksym.yaml) to hold records of symlinks.
+    Flags:
+      -u, --update
+        Update the init_directory field in the .linksym.yaml configuration file.
 
-	fmt.Println("\n Flags:")
-	fmt.Println("   -h, --help")
-	fmt.Println("     Print this help message")
+  add [path] [(optional) destination]
+    Create a symlink for the specified path. Optionally define a destination for the symlink.
+
+  remove [path]
+    Remove the symlink and restore the original file to its original path.
+
+  source
+    Create all symlinks described in the .linksym.yaml configuration file.
+
+Flags:
+  -h, --help
+    Display this help message.`
+
+	fmt.Println(usage)
 }
