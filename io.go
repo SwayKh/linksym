263536c3ca048db1006efe9d200a6a09cd463d16
SwayKh <khatrisumit712@gmail.com>
2024-10-02 19:50:31 +0530

    Move Initialising config logic to init.go

--- io.go
+++ io.go
@@ -60,34 +60,3 @@ func WriteConfig(configuration *AppConfig) error {
 	}
 	return nil
 }
-
-// Create a default config file with empty records and Current working directory
-// variable for Init directory
-func InitialiseConfig(configPath string) error {
-	InitDirectory, err := os.Getwd()
-	if err != nil {
-		return fmt.Errorf("Couldn't get the current working directory")
-	}
-
-	InitDirectory = AliasPath(InitDirectory, true)
-
-	configuration := AppConfig{
-		InitDirectory: InitDirectory,
-		Records:       []record{},
-	}
-
-	AliasConfig(&configuration)
-	data, err := yaml.Marshal(configuration)
-	if err != nil {
-		return fmt.Errorf("Error marshalling data from configuration{}: %w", err)
-	}
-
-	err = os.WriteFile(configPath, data, 0o644)
-	if err != nil {
-		return fmt.Errorf("Error writing record to config file: %w", err)
-	}
-
-	Log("Initialising %s file in the current directory.", filepath.Base(configPath))
-
-	return nil
-}
