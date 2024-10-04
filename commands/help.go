1c7109bf0263f997b20cd42b1c53465194b8877e
SwayKh <khatrisumit712@gmail.com>
2024-10-04 22:36:02 +0530

    Update Help Message function to use color package and Bold/Underline formatting. Really creates a Linux utils --help page look

--- commands/help.go
+++ commands/help.go
@@ -1,33 +1,37 @@
 package commands
 
-import "github.com/SwayKh/linksym/logger"
+import (
+	"github.com/fatih/color"
+)
 
 func Help() {
-	usage := ` USAGE:
-  linksym [flags] [subcommand]
+	white := color.New(color.FgWhite).PrintlnFunc()
+	boldWhite := color.New(color.FgWhite, color.Bold).PrintlnFunc()
+	underlineBoldWhite := color.New(color.FgWhite, color.Bold, color.Underline).PrintlnFunc()
 
-FLAGS:
-  -h, --help
-    Display this help message.
-  -v
-    Show verbose output.
-
-AVAILABLE COMMANDS:
-  init
-    Initialize the linksym configuration file (.linksym.yaml) to hold records of symlinks.
-
-  add [path] [destination (Optional) ]
-    Create a symlink for the specified path. Optionally takes a destination path for the symlink.
-
-  remove [path]
-    Remove the symlink and restore the original file to its original path.
-
-  source
-    Create all symlinks described in the .linksym.yaml configuration file.
-
-  update
-    Update the .linksym.yaml configuration file in the current directory.
-`
-
-	logger.Log(logger.WARNING, usage)
+	underlineBoldWhite("USAGE:")
+	boldWhite("  linksym [flags] [subcommand]")
+	white()
+	underlineBoldWhite("FLAGS:")
+	boldWhite("  -h, --help")
+	white("    Display this help message.")
+	boldWhite("  -v")
+	white("    Show verbose output.")
+	white()
+	underlineBoldWhite("AVAILABLE COMMANDS:")
+	boldWhite("  init")
+	white("    Initialize the linksym configuration file (.linksym.yaml) to hold records of symlinks.")
+	white()
+	boldWhite("  add [path] [destination (Optional) ]")
+	white("    Create a symlink for the specified path. Optionally takes a destination path for the symlink.")
+	white()
+	boldWhite("  remove [paths]")
+	white("    Remove the symlink and restore the original file to its original path.")
+	white()
+	boldWhite("  source")
+	white("    Create all symlinks described in the .linksym.yaml configuration file.")
+	white()
+	boldWhite("  update")
+	white("    Update the .linksym.yaml configuration file in the current directory.")
+	white()
 }
