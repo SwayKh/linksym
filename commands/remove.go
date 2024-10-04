8b9fb9751e1e74eec4d227dfe4763a08dc9f2679
SwayKh <khatrisumit712@gmail.com>
2024-10-04 17:25:36 +0530

    Update Remove subcommand to take multiple arguments and remove each record in one go

--- commands/remove.go
+++ commands/remove.go
@@ -11,17 +11,16 @@ import (
 
 // Get the absolute path of "LinkName", which should be the path relative from
 // the Init Directory, and Find the matching LinkName through the []Records in
-// .linksym.yaml, UnLink it, and Remove from the []Records. Expects one
-// argument, and throws error on multiple arguments provided
+// .linksym.yaml, UnLink it, and Remove from the []Records. can take multiple
+// arguments and looks over them Removing each one
 func (app *Application) Remove(args []string) error {
-	switch len(args) {
-	case 1:
+	for _, path := range args {
 		var sourcePath, destinationPath string
 		var err error
 		var found bool
 
 		// Get the File Info of LinkName provided from the arguments
-		linkInfo, err := config.GetFileInfo(args[0])
+		linkInfo, err := config.GetFileInfo(path)
 		if err != nil {
 			return err
 		} else if !linkInfo.Exists {
@@ -31,15 +30,14 @@ func (app *Application) Remove(args []string) error {
 		logger.Log("Unlinking %s", config.AliasPath(linkInfo.AbsPath, app.HomeDirectory, app.InitDirectory, true))
 
 		// Since the "filename" of the record can be the same with a different file,
-		// just linked in separate directories, getting the filename and the above
+		// just linked in separate directories, getting the filename and the parent
 		// directory name should make the name unique enough to be checked in
 		// []Records
-		recordPathName := filepath.Join(filepath.Base(filepath.Dir(linkInfo.AbsPath)), filepath.Base(linkInfo.AbsPath))
+		fileName := filepath.Base(linkInfo.AbsPath)
+		dirName := filepath.Base(filepath.Dir(linkInfo.AbsPath))
+		recordPathName := filepath.Join(dirName, fileName)
 
-		// Can't use range over Configuration.Records. since the slice gets modified
-		// during iteration with the RemoveRecord function, so iterate over the
-		// slice manually in reverse order to fix this
-		for i := len(app.Configuration.Records) - 1; i >= 0; i-- {
+		for i := range app.Configuration.Records {
 			if app.Configuration.Records[i].Name == recordPathName {
 				sourcePath = app.Configuration.Records[i].Paths[0]
 				destinationPath = app.Configuration.Records[i].Paths[1]
@@ -58,9 +56,6 @@ func (app *Application) Remove(args []string) error {
 		}
 
 		app.Configuration.RemoveRecord(recordPathName)
-
-	default:
-		return fmt.Errorf("Invalid number of arguments")
 	}
 	return nil
 }
