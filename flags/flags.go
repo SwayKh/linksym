8813c5c90911de1d98305bab2e5d722f02af0811
SwayKh <khatrisumit712@gmail.com>
2024-10-04 17:26:06 +0530

    Remove unneeded flagSet commands for Add,Remove,Init,Source,Update commands

--- flags/flags.go
+++ flags/flags.go
@@ -4,12 +4,11 @@ import (
 	"flag"
 )
 
+// Flags are in a different package, since the logger package needs the
+// VerboseFlag. Moving the flags logic to commands/linksym.go causes import
+// cycle since the subcommands needs logger package and Logger package would
+// import commands
 var (
-	AddFlag     *flag.FlagSet
-	RemoveFlag  *flag.FlagSet
-	InitFlag    *flag.FlagSet
-	SourceFlag  *flag.FlagSet
-	UpdateFlag  *flag.FlagSet
 	HelpFlag    *bool
 	VerboseFlag *bool
 )
@@ -19,12 +18,5 @@ func CreateFlags() {
 	// Handle both -h and --help with one boolean
 	HelpFlag = flag.Bool("h", false, "Show help")
 	flag.BoolVar(HelpFlag, "help", false, "Show help")
-
 	VerboseFlag = flag.Bool("v", false, "Verbose output")
-
-	AddFlag = flag.NewFlagSet("add", flag.ExitOnError)
-	RemoveFlag = flag.NewFlagSet("remove", flag.ExitOnError)
-	InitFlag = flag.NewFlagSet("init", flag.ExitOnError)
-	SourceFlag = flag.NewFlagSet("source", flag.ExitOnError)
-	UpdateFlag = flag.NewFlagSet("update", flag.ExitOnError)
 }
